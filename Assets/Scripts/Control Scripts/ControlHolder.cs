d746fbd63df90b63e7e8d8f7fc84e919b5be2484
Leigh <leighpri@gmail.com>
2016-06-20 15:08:43 -0400

    fixed erroneus deletion of Controls GameObject

--- Assets/Scripts/Control Scripts/ControlHolder.cs
+++ Assets/Scripts/Control Scripts/ControlHolder.cs
@@ -1,19 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-//this class makes sure that the object it's placed on doesn't destroy on load. And stays static
-//It is for static container objects that require no behavior of their own.
-public class DontDestroy : MonoBehaviour {
+public class ControlHolder : MonoBehaviour {
 
-    public static DontDestroy controlHolder;
+    public static ControlHolder controlHolder;
 
     void Awake() {
         if (controlHolder == null) {
-            controlHolder = this;
             DontDestroyOnLoad(gameObject);
+            controlHolder = this;
         }
         else if (controlHolder != this)
+        {
             Destroy(gameObject);
+        }
     }
 
     // Use this for initialization
