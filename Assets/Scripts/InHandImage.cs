2a2eb97cc147d37dede9fede107c27eb526025a1
Leigh <leighpri@gmail.com>
2016-07-18 13:04:20 -0400

    Updated JSON files

---
+++ Assets/Scripts/InHandImage.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class InHandImage : MonoBehaviour {
+
+    public static InHandImage inHandImage;
+
+    void Awake() {
+        if (!InHandCtrl.isInHand)
+            gameObject.SetActive(false);
+    }
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (InHandCtrl.isInHand) {
+            if (InHandCtrl.typeOfObject == (int)ObjectMaster.listType.Building)
+                GetComponentInChildren<Image>().sprite = InHandCtrl.inHandCtrl.buildingInHand.spriteArray[InHandCtrl.objectInHand];
+        }
+    }
+
+    void OnMouseUpAsButton() {
+        InHandCtrl.inHandCtrl.CancelInHand();
+    }
+}
