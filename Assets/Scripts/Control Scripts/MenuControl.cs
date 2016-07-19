d9e9ffef7532baa2894fccf61a6b03337df6b53c
Leigh <leighpri@gmail.com>
2016-07-19 11:31:51 -0400

    added scrolling to menus and reactive menu sizes

--- Assets/Scripts/Control Scripts/MenuControl.cs
+++ Assets/Scripts/Control Scripts/MenuControl.cs
@@ -10,27 +10,35 @@ public class MenuControl : MonoBehaviour {
     public ObjectMaster objMaster;
     public GameObject exampleMenuObject;
     public GameObject[] menuObjects;
+    public GameObject scrollView;
+
+    //change just these values to change the menu sizes for all menus
+    private Vector3 masterInitialPos = new Vector3(-171, 143, 0);
+    private float masterOffset = 29.4f;
 
     void OnLevelWasLoaded(int level) {
         if (level == 3)
             FillMenuWithBuildings();
         else if (level == 4)
             FillMenuWithGrapes();
+        else if (level == 5)
+            FillMenuWithVines();
         else if (level == 6)
             SellMenu();
     }
 
-    void Update() {
+    void Start() {
+        scrollView.gameObject.transform.SetParent(gameObject.transform);
     }
 
     private void FillMenuWithBuildings() {
         menuObjects = new GameObject[ObjectMaster.buildingList.Count];
-        Vector3 initialPos = new Vector3(-171,143,0);
-        float yOffset = 29.4f;
+        Vector3 initialPos = masterInitialPos;
+        float yOffset = masterOffset;
         for (int i = 0; i < ObjectMaster.buildingList.Count; i++) {
             int tempID = ObjectMaster.buildingList[i].id;
             menuObjects[i] = Instantiate(exampleMenuObject, initialPos, Quaternion.identity) as GameObject; 
-            menuObjects[i].transform.SetParent(gameObject.transform, false);
+            menuObjects[i].transform.SetParent(scrollView.gameObject.transform, false);
             //displays name of building
             menuObjects[i].GetComponent<Text>().text = ObjectMaster.buildingList[i].objectName;
             //displays cost of building in child of object
@@ -46,12 +54,12 @@ public class MenuControl : MonoBehaviour {
 
     private void FillMenuWithGrapes() {
         menuObjects = new GameObject[ObjectMaster.consumableList.Count];
-        Vector3 initialPos = new Vector3(-171, 143, 0);
-        float yOffset = 29.4f;
+        Vector3 initialPos = masterInitialPos;
+        float yOffset = masterOffset;
         for (int i = 0; i < ObjectMaster.consumableList.Count; i++) {
             int tempID = ObjectMaster.consumableList[i].id;
             menuObjects[i] = Instantiate(exampleMenuObject, initialPos, Quaternion.identity) as GameObject;
-            menuObjects[i].transform.SetParent(gameObject.transform, false);
+            menuObjects[i].transform.SetParent(scrollView.gameObject.transform, false);
             //displays name of building
             menuObjects[i].GetComponent<Text>().text = ObjectMaster.consumableList[i].objectName;
             //displays cost of building in child of object
@@ -65,15 +73,36 @@ public class MenuControl : MonoBehaviour {
         }
     }
 
+    private void FillMenuWithVines() {
+        menuObjects = new GameObject[ObjectMaster.vineList.Count];
+        Vector3 initialPos = masterInitialPos;
+        float yOffset = masterOffset;
+        for (int i = 0; i < ObjectMaster.vineList.Count; i++) {
+            int tempID = ObjectMaster.vineList[i].id;
+            menuObjects[i] = Instantiate(exampleMenuObject, initialPos, Quaternion.identity) as GameObject;
+            menuObjects[i].transform.SetParent(scrollView.gameObject.transform, false);
+            //displays name of building
+            menuObjects[i].GetComponent<Text>().text = ObjectMaster.vineList[i].objectName;
+            //displays cost of building in child of object
+            menuObjects[i].gameObject.transform.GetChild(0).gameObject.GetComponent<Text>().text = ObjectMaster.vineList[i].cost.ToString() + "g";
+            //enables button functionality
+            menuObjects[i].gameObject.AddComponent<Button>().onClick.AddListener(delegate () { objMaster.BuyVine(tempID); });
+            //stores initialPos over again so that the offsets are maintained
+            initialPos = new Vector3(initialPos.x, //x
+                                    initialPos.y - yOffset, //y, with an offset
+                                     initialPos.z); //z
+        }
+    }
+
     private void SellMenu() { 
         menuObjects = new GameObject[ObjectMaster.wineList.Count];
-        Vector3 initialPos = new Vector3(-171, 143, 0);
-        float yOffset = 29.4f;
+        Vector3 initialPos = masterInitialPos;
+        float yOffset = masterOffset;
         for (int i = 0; i < ObjectMaster.wineList.Count; i++) {
             if (ObjectMaster.wineList[i].bottlesOnHand > 0) { //only populates list if there are bottles available to sell
                 int tempID = ObjectMaster.consumableList[i].id;
                 menuObjects[i] = Instantiate(exampleMenuObject, initialPos, Quaternion.identity) as GameObject;
-                menuObjects[i].transform.SetParent(gameObject.transform, false);
+                menuObjects[i].transform.SetParent(scrollView.gameObject.transform, false);
                 //displays name of building
                 menuObjects[i].GetComponent<Text>().text = ObjectMaster.wineList[i].wineName;
                 //displays number of bottles available
